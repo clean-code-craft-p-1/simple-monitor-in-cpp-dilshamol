1602a579161f125d6ae38f07b30416fa4a982494
dilshamol <140040094+dilshamol@users.noreply.github.com>
2023-08-31 12:52:54 +0530

    Update checker.cpp

--- checker.cpp
+++ checker.cpp
@@ -3,21 +3,9 @@
 #include <unistd.h>
 using namespace std;
 
-int vitalsOk(float temperature, float pulseRate, float spo2) {
-  if(temperature > 102 || temperature < 95) {
-    cout << "Temperature critical!\n";
-    for (int i = 0; i < 6; i++)
-    {
-      cout << "\r* " << flush;
-      sleep(1);
-      cout << "\r *" << flush;
-      sleep(1);
-    }
-
-    return 0;
-  } else if(pulseRate < 60 || pulseRate > 100) {
-    cout << "Pulse Rate is out of range!\n";
-    for (int i = 0; i < 6; i++)
+int callSleep()
+{
+     for (int i = 0; i < 6; i++)
     {
       cout << "\r* " << flush;
       sleep(1);
@@ -25,22 +13,53 @@ int vitalsOk(float temperature, float pulseRate, float spo2) {
       sleep(1);
     }
     return 0;
-  } else if(spo2 < 90) {
-    cout << "Oxygen Saturation out of range!\n";
-    for (int i = 0; i < 6; i++)
-    {
-      cout << "\r* " << flush;
-      sleep(1);
-      cout << "\r *" << flush;
-      sleep(1);
-    }
-    return 0;
-  }
-  return 1;
 }
 
-int main() {
+int isTemperatureOk(float temperature)
+{
+    if(temperature > 102 || temperature <95)
+    {
+        cout<<"Temperature critical!\n";
+        callSleep();
+        return 0;
+    }
+    return 1;
+
+}
+
+int isPulseRateOk(float pulseRate)
+{
+    if(pulseRate < 60 || pulseRate > 100)
+    {
+        cout<<"Pulse Rate is out of range!\n";
+        callSleep();
+        return 0;
+    }
+    return 1;
+}
+
+int isSpo2Ok(float spo2)
+{
+    if(spo2 < 90)
+    {
+        cout << "Oxygen Saturation out of range!\n";
+        callSleep();
+        return 0;
+    }
+    return 1;
+}
+
+int vitalsOk(float temperature, float pulseRate, float spo2) 
+{
+     !isTemperatureOk(temperature) || !isPulseRateOk(pulseRate) || !isSpo2Ok(spo2);
+}
+
+int main() 
+{
   assert(!vitalsOk(99, 102, 70));
+  assert(!vitalsOk(90, 80, 100));
+  assert(!vitalsOk(105, 80, 100));
+  assert(!vitalsOk(96, 70, 85));
   assert(vitalsOk(98.1, 70, 98));
   cout << "Done\n";
 }
